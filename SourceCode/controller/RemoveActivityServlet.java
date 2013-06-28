92b23a03fab7fce61f2ad290f60c9ee593c30454
tangye <sheng02002@sina.com>
2013-06-28 21:12:38 +0800

    Update RemoveActivityServlet.java

--- SourceCode/controller/RemoveActivityServlet.java
+++ SourceCode/controller/RemoveActivityServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;//导入需要的包
 
 public class RemoveActivityServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -22,5 +22,5 @@ public class RemoveActivityServlet extends HttpServlet{
 			throws ServletException, IOException {
 		
 	
-	}
+	}//对异常的处理
 }
